129a6fede0a2f4b10b366a5cacd564d0bdfa3b58
HUSTyankang <1227752726@qq.com>
2019-09-08 10:52:29 +0800

    添加了自定义的查询规则。

--- microservice-springcloud-consumer-dept-80/src/main/java/com/hust/springcloud/ConsumerApplication80.java
+++ microservice-springcloud-consumer-dept-80/src/main/java/com/hust/springcloud/ConsumerApplication80.java
@@ -1,11 +1,16 @@
 package com.hust.springcloud;
 
+import com.hust.myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//在启动该微服务时就能去加载我们的自定义Ribbon配置类，从而使配置生效
+//自定义的Ribbon配置类不能放在@ComponentScan所扫描的当前包及子包下，否则该自定义类就会被所有的Ribbon客户端所共享。
+@RibbonClient(name = "MICROSERVICE-SPRINGCLOUD-DEPT",configuration = MyselfRule.class)
 public class ConsumerApplication80 {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication80.class,args);
--- microservice-springcloud-consumer-dept-80/src/main/java/com/hust/springcloud/configbeans/ConfigBean.java
+++ microservice-springcloud-consumer-dept-80/src/main/java/com/hust/springcloud/configbeans/ConfigBean.java
@@ -2,6 +2,7 @@ package com.hust.springcloud.configbeans;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
 import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +19,9 @@ public class ConfigBean {
 
     @Bean
     public IRule myRule(){
-       return new RandomRule(); //用随机算法替代默认的轮询算法。
+//       return new RandomRule(); //用随机算法替代默认的轮询算法。
+
+        //先轮询，如果服务宕机，则会再指定的时间内重试，获取可用的服务。
+        return new RetryRule();
     }
 }
